03ed77e7ef11fbec844583a404a97479ee3ca95c
mokhiya <mohiyaxonuzokova@gmail.com>
2024-11-16 19:57:38 +0500

    commit

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from posts.models import PostModel, CommentModel, CommentLikeModel, PostLikeModel
+
+admin.site.register(PostModel)
+admin.site.register(CommentModel)
+admin.site.register(CommentLikeModel)
+admin.site.register(PostLikeModel)
+
--- posts/models.py
+++ posts/models.py
@@ -20,8 +20,49 @@ class PostModel(models.Model):
         verbose_name = 'Post'
         verbose_name_plural = 'Posts'
 
-# class LikeModel(models.Model):
-#     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='likes')
-#     post = models.ForeignKey(PostModel, on_delete=models.CASCADE, related_name='likes')
-#     created_at = models.DateTimeField(auto_now_add=True)
+
+class CommentModel(models.Model):
+    post = models.ForeignKey(PostModel, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    image = models.ImageField(upload_to='comments', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} - {self.text[:20]}"
+
+    class Meta:
+        ordering = ['created_at']
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
+
+
+class PostLikeModel(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='post_likes')
+    post = models.ForeignKey(PostModel, on_delete=models.CASCADE, related_name='post_likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} liked {self.post.id}"
+
+    class Meta:
+        ordering = ['created_at']
+        verbose_name = 'PostLike'
+        verbose_name_plural = 'PostLikes'
+        unique_together = (('user', 'post'),)
+
+
+class CommentLikeModel(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='comment_likes')
+    comment = models.ForeignKey(CommentModel, on_delete=models.CASCADE, related_name='comment_likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} liked comment {self.comment.id}"
+
+    class Meta:
+        ordering = ['created_at']
+        verbose_name = 'CommentLike'
+        verbose_name_plural = 'CommentLikes'
+        unique_together = (('user', 'comment'),)
 
